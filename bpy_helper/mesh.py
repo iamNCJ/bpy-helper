a10d68f72c7c21d7fe2035c3cd49ec069a6b62c5
NCJ <me@ncj.wiki>
2025-01-07 01:12:30 +0800

    Update mesh.py

--- bpy_helper/mesh.py
+++ bpy_helper/mesh.py
@@ -3,19 +3,21 @@ import bpy
 from bpy_helper.scene import reset_scene, import_3d_model, normalize_scene, scene_meshes
 
 
-def uv_unwarp(
+def uv_unwrap(
     input_mesh_file: str,
     output_mesh_file: str,
-    method: Literal['unwarp', 'smart_project', 'cube_project', 'cylinder_project', 'sphere_project'] = 'unwarp',
+    method: Literal['unwrap', 'smart_project', 'cube_project', 'cylinder_project', 'sphere_project'] = 'unwrap',
+    normalize: bool = True,
     **kwargs
 ) -> bool:
     """
-    Unwarps the UVs of the given mesh file.
+    Unwraps the UVs of the given mesh file.
 
     Args:
         input_mesh_file (str): The path to the input mesh file.
         output_mesh_file (str): The path to the output mesh file.
-        method (Literal['unwarp', 'smart_project', 'cube_project', 'cylinder_project', 'sphere_project'], optional): The method to use for unwrapping. Defaults to 'unwarp'.
+        method (Literal['unwrap', 'smart_project', 'cube_project', 'cylinder_project', 'sphere_project'], optional): The method to use for unwrapping. Defaults to 'unwrap'.
+        normalize (bool, optional): Whether to normalize the scene before unwrapping. Defaults to True.
         kwargs: Additional keyword arguments.
 
     Returns:
@@ -32,14 +34,15 @@ def uv_unwarp(
         obj.rotation_euler = (0.0, 0.0, 0.0)
 
     # normalize the scene
-    scale, offset = normalize_scene(use_bounding_sphere=True)
-    print(f"scale: {scale}, offset: {offset}")
+    if normalize:
+        scale, offset = normalize_scene(use_bounding_sphere=True)
+        print(f"scale: {scale}, offset: {offset}")
 
     bpy.ops.object.select_all(action='DESELECT')
     bpy.ops.object.mode_set(mode='EDIT')
     bpy.ops.mesh.select_all(action='SELECT')
 
-    if method == 'unwarp':
+    if method == 'unwrap':
         _kwargs = {
             'method': 'ANGLE_BASED',
             'margin': 0.001
